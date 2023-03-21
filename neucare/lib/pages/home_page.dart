c739bd03465f5644164d6f1652c4b210698727ec
Amoghk04 <kalasapuraamogh@gmail.com>
2023-03-21 22:48:12 +0530

    neucare: home_page and signin_page do same work.

--- neucare/lib/pages/home_page.dart
+++ neucare/lib/pages/home_page.dart
@@ -1,21 +1,19 @@
-import 'package:flutter/material.dart';
-import 'package:neucare/pages/top_bar.dart';
+//import 'package:flutter/material.dart';
+//import 'package:neucare/pages/top_bar.dart';
 
-class HomePage extends StatelessWidget {
-  final String userName;
-  const HomePage({super.key, required this.userName});
+//class HomePage extends StatelessWidget {
+  //const HomePage({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        backgroundColor: Colors.grey[300],
-        body: Column(
-          children: [
-            TopBar(
-              imagePath: 'lib/images/token.png',
-              userName: userName,
-            ),
-          ],
-        ));
-  }
-}
+  //@override
+  //Widget build(BuildContext context) {
+    //return Scaffold(
+      //  backgroundColor: Colors.grey[300],
+        //body: Column(
+          //children: const [
+            //TopBar(
+              //imagePath: 'lib/images/token.png',
+            //),
+          //],
+        //));
+  //}
+//}
