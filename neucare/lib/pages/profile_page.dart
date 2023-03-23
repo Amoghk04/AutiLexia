1b779e1ef32a2add33b4f0e105959092ffdf42ac
Amoghk04 <kalasapuraamogh@gmail.com>
2023-03-23 13:48:55 +0530

    neucare: basic profile layout completed.

--- neucare/lib/pages/profile_page.dart
+++ neucare/lib/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:neucare/pages/app_bar.dart';
 import 'package:neucare/pages/custom_container.dart';
+import 'package:neucare/pages/login_register_page.dart';
 
 class ProfilePage extends StatelessWidget {
   final User? user;
@@ -12,8 +13,39 @@ class ProfilePage extends StatelessWidget {
     return customContainer(Scaffold(
       backgroundColor: Colors.transparent,
       appBar: customAppBar(0, Colors.lightBlue[50], Colors.black, "Profile"),
-      // TODO: Write the profile page styling in the body of this Scaffold.
-      body: const SizedBox(height: 20),
+      // Write the profile page styling in the body of this Scaffold.
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            children: [
+              SizedBox(
+                width: 120,
+                height: 120,
+                child: Image.asset("lib/images/profile_placeholder.png"),
+              ),
+              const SizedBox(height: 20),
+              Text('User Name',
+                  style: Theme.of(context).textTheme.headlineSmall),
+              const SizedBox(height: 15),
+              Text('${user?.email}',
+                  style: Theme.of(context).textTheme.bodyLarge),
+              const SizedBox(height: 40),
+              SizedBox(
+                height: 120,
+                width: 120,
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.asset("lib/images/pet.png")),
+              ),
+              const SizedBox(height: 20),
+              Text('Pet Name',
+                  style: Theme.of(context).textTheme.headlineSmall),
+              const Divider(),
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
