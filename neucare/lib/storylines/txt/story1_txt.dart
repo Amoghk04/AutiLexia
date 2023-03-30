6daf8769dbe1879dc25fa7af96972416242ed1d4
Amoghk04 <kalasapuraamogh@gmail.com>
2023-03-30 16:25:20 +0530

    neucare: need to add the story part.

--- neucare/lib/storylines/txt/story1_txt.dart
+++ neucare/lib/storylines/txt/story1_txt.dart
@@ -1,5 +1,5 @@
 const List<String> story = [
-  "This is a paragraph 1.",
+  "This is the story about a boy who suffered from dyslexia, he had a pet named blah blah....",
   "This is a paragraph 2.",
   "This is a paragraph 3.",
 ];
