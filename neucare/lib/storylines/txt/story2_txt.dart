3e74795bff402e6232baeaddf919ae3ce1e677c7
Amoghk04 <kalasapuraamogh@gmail.com>
2023-04-02 14:38:30 +0530

    neucare: Added the second story

--- neucare/lib/storylines/txt/story2_txt.dart
+++ neucare/lib/storylines/txt/story2_txt.dart
@@ -1,21 +1,39 @@
 const List<String> story2Question1 = [
-  "",
+  "Hi there, nice to meet you, I am Miss Unicorn and I'm a very close friend of Mr. Phoenix",
+  "I am going to tel you the story of Claudio the turtle",
+  "On the bank of the river Snockerdoodle, lived a turtle by the name Claudio. His parents were Mr. Mario and Mrs. Marinara",
+  "Claudio had a hear of gold and was a good and extroverted turtle but he was different from the others. He had difficulty in grasping things quickly which resulted in some trouble in his academics.",
+  "His parents were worried that he was falling behind in his studies due to some kind of a problem and took him to a doctor",
+  "The doctor diagnosed him with dyslexia, and advised him to focus a lot more on speaking, reading, writing and listening skills.",
+  "Here are a few instances, through which the doctor was able to identify he had dyslexia, try to get the correct answer as to how Claudio would have reacted in these situations.",
+  "One day in school, Claudio's teacher Mrs. Suzie gave the students a dictation test, one of the words was 'ball', what do you think Claudio wrote its spelling as?",
 ];
 
-const String story2Question1Correct = "";
+const String story2Question1Correct =
+    "That’s amazing. People with dyslexia have confusion over letters that look similar and put the letters the wrong way around such as writing d instead of b.";
 
-const String story2Question1Wrong = "";
+const String story2Question1Wrong =
+    "Not quite! People with dyslexia have confusion over letters that look similar and put the letters the wrong way around such as writing d instead of b.";
 
 const List<String> story2Options1 = [
-  "",
+  "A. Claudio wrote the word ball without any mistakes.",
+  "B. Claudio left the classroom",
+  "C. Claudio didnt write the word",
+  "D. Claudio wrote dall instead of ball.",
 ];
 
 const List<String> story2Question2 = [
-  "",
+  "On another day, a few weeks later..., in the library period in his school, all the students were asked to pick a book",
+  "All the students took the book they wanted to read, but Claudio went and sat in the corner solemnly without anything in his hand",
+  "The librarian saw this and asked what the problem was",
+  "How do you think Claudio replied to it?",
 ];
 
 const List<String> story2Options2 = [
-  "",
+  "He didn't say anything and just took a random book and acted like reading it",
+  "He ran away without giving any reply to the librarian",
+  "He ignored the librarian and sat without doing anything",
+  "He told his librarian he doesn't like to read and asked whether he could go out and playt",
 ];
 
 const String story2Question2Correct = "";
