e0f72aba6fb1b6d8433d2704e1d1a2613aec58bf
sathya-pramodh <sathyapramodh17@gmail.com>
2023-04-04 13:50:20 +0530

    spectramind: Added remaining part Dyslexia storyline.

--- spectramind/lib/storylines/txt/story2_txt.dart
+++ spectramind/lib/storylines/txt/story2_txt.dart
@@ -30,40 +30,49 @@ const List<String> story2Question2 = [
 ];
 
 const List<String> story2Options2 = [
-  "He didn't say anything and just took a random book and acted like reading it",
-  "He ran away without giving any reply to the librarian",
-  "He ignored the librarian and sat without doing anything",
-  "He told his librarian he doesn't like to read and asked whether he could go out and playt",
+"A.	Claudio chose Harry potter to read.",
+"B.	Claudio told his librarian that he doesn’t like to read and asked whether he could go out and play.",
+"C.	Claudio tore the book his librarian gave him",
+"D.	Claudio hid behind a shelf of books",
 ];
 
-const String story2Question2Correct = "";
+const String story2Question2Correct = "Amazing job! People with dyslexia do not prefer reading as it frustrates them. This is why they must be taught in more effective and innovative ways so that they come back to enjoy reading.";
 
-const String story2Question2Wrong = "";
+const String story2Question2Wrong = "Not quite! People with dyslexia do not prefer reading as it frustrates them. This is why they must be taught in more effective and innovative ways so that they come back to enjoy reading.";
 
 const List<String> story2Question3 = [
-  "",
+  "The teacher Mrs. Suzie told the kids to remember the tables of 5 having explained the children a lot of times. How do you think Claudio did?",
 ];
 
-const String story2Question3Correct = "";
+const String story2Question3Correct = "Excellent work! People with dyslexia have trouble remembering and memorising. It must be ensured that they are made to love the subject and instead of memorising, help them remember things.";
 
-const String story2Question3Wrong = "";
+const String story2Question3Wrong = "Not quite! People with dyslexia have trouble remembering and memorising.It must be ensured that they are made to love the subject and instead of memorising, help them remember things.";
 
 const List<String> story2Options3 = [
-  "",
+"A.	Claudio is very efficient with tables of 5",
+"B.	Claudio fails to remember tables of 5.",
+"C.	Claudio answers back to his teacher.",
+"D.	Claudio left the class.",
 ];
 
 const List<String> story2Question4 = [
-  "",
+"It was exam time and the paper was super easy. Everyone finished the paper in an average of 30 min. How do you think Claudio likely did on the test?"
 ];
 
-const String story2Question4Correct = "";
+const String story2Question4Correct = "Amazing! People with dyslexia find it hard to finish things on time and take more time to do their work or don’t finish their work in the stipulated timeframe.";
 
-const String story2Question4Wrong = "";
+const String story2Question4Wrong = "Not quite! People with dyslexia find it hard to finish things on time and take more time to do their work or don’t finish their work in the stipulated timeframe.";
 
 const List<String> story2Options4 = [
-  "",
+"A.	Claudio did super awesome and finished the test in 15 min.",
+"B.	Claudio couldn’t finish the test in time.",
+"C.	Claudio retaliated at his teacher for giving the test.",
+"D.	Claudio slept during the test.",
 ];
 
 const List<String> story2Outro = [
-  "",
+"This is the story of what Claudio goes through at his school. Congratulations on helping Dr. Basu diagnose him very well and helping Claudio get the best treatment possible!",
+"We need more people like you who understand other people and oneself helping everyone lead a fruitful life. ",
+"People with dyslexia often show these symptoms of confusion, not being able to recollect, spelling problems, avoiding reading being clumsy and more. ",
+"There is lots of help out there in this app and in the world for such people and don’t miss out!!! I will meet you again in the stories in the future! We love you!",
 ];
