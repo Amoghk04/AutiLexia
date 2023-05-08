eb0c4cd02289e25b1fcd87b1c31cc6837d4dff91
sathya-pramodh <sathyapramodh17@gmail.com>
2023-05-08 21:52:36 +0530

    daily_challenge: Added core functionality. Challenges to go.

--- spectramind/lib/db.dart
+++ spectramind/lib/db.dart
@@ -15,4 +15,14 @@ class DatabaseManager {
         FirebaseFirestore.instance.collection('users').doc(uid);
     await userDoc.update({"tokens": tokens});
   }
+
+  Future<void> updateUserLastDailyChallenge(
+      {required String? email, required String? lastDailyChallenge}) async {
+    CollectionReference users = FirebaseFirestore.instance.collection('users');
+    QuerySnapshot querySnap = await users.where("name", isEqualTo: email).get();
+    String uid = querySnap.docs[0].id;
+    DocumentReference userDoc =
+        FirebaseFirestore.instance.collection('users').doc(uid);
+    await userDoc.update({'last_challenge': lastDailyChallenge});
+  }
 }
--- spectramind/lib/storylines/daily_challenge.dart
+++ spectramind/lib/storylines/daily_challenge.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:intl/intl.dart';
 import 'package:spectramind/components/app_bar.dart';
 import 'package:spectramind/components/custom_container.dart';
+import 'package:spectramind/db.dart';
 
 class DailyChallenge extends HookWidget {
   final User? user;
@@ -10,9 +13,27 @@ class DailyChallenge extends HookWidget {
   @override
   Widget build(BuildContext context) {
     final challengeCompleted = useState(false);
+    final formattedCurrentDate = useState("");
     useEffect(() {
-      // TODO: Get from database if the user has completed their daily challenge or not.
-      challengeCompleted.value = false;
+      DateTime currentDate = DateTime.now();
+      formattedCurrentDate.value = DateFormat.yMMMd().format(currentDate);
+      FirebaseFirestore.instance
+          .collection('users')
+          .where('name', isEqualTo: user?.email)
+          .get()
+          .then((matches) async {
+        String? lastDailyChallenge = matches.docs[0].get('last_challenge');
+        if (lastDailyChallenge == null) {
+          challengeCompleted.value = false;
+          await DatabaseManager().updateUserLastDailyChallenge(
+              email: user?.email,
+              lastDailyChallenge: formattedCurrentDate.value);
+        } else if (lastDailyChallenge == formattedCurrentDate.value) {
+          challengeCompleted.value = true;
+        } else {
+          challengeCompleted.value = false;
+        }
+      });
       return null;
     }, []);
     return customContainer(
@@ -25,17 +46,30 @@ class DailyChallenge extends HookWidget {
               child: customAppBar(Colors.lightBlue[50], Colors.black, user),
             ),
             (!challengeCompleted.value)
-                ? const Text("\n\nStay tuned for the daily challenges🕰🕰.",
-                    style: TextStyle(
-                      fontFamily: "LuckiestGuy",
-                      fontSize: 20,
-                    ))
-                : const Text(
-                    "\n\nDaily Challenge completed! Come back tomorrow!",
-                    style: TextStyle(
-                      fontFamily: "LuckiestGuy",
-                      fontSize: 20,
-                    ))
+                ? ElevatedButton(
+                    onPressed: () {
+                      DatabaseManager()
+                          .updateUserLastDailyChallenge(
+                              email: user?.email,
+                              lastDailyChallenge: formattedCurrentDate.value)
+                          .then((_) {
+                        Navigator.of(context).pop();
+                      });
+                    },
+                    child: const Text("Submit"))
+                : Column(children: const [
+                    SizedBox(height: 50),
+                    Text("Daily Challenge Completed!",
+                        style: TextStyle(
+                          fontFamily: 'LuckiestGuy',
+                          fontSize: 20,
+                        )),
+                    Text("Come back tomorrow!",
+                        style: TextStyle(
+                          fontFamily: 'LuckiestGuy',
+                          fontSize: 20,
+                        ))
+                  ])
           ]),
         ),
       ),
