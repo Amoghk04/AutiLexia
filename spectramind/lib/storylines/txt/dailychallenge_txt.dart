b60cdd265d4c098b6d5834a9bf63e9b03bce9d29
Amoghk04 <kalasapuraamogh@gmail.com>
2023-05-18 12:33:49 +0530

    spectramind: Added from index and dc_txt pages

---
+++ spectramind/lib/storylines/txt/dailychallenge_txt.dart
@@ -0,0 +1,33 @@
+const List<String> dailychallenge1question1 = [
+  "1. Tina the squirrel has collected 3 hazelnuts for winter. Her mom Squeaky gives her 4 more hazelnuts. How many hazelnuts does Tina have now",
+];
+
+const String dailychallenge1question1Correct =
+    "Yes, You are correct Tina has 7 hazelnuts 4 from her mom and 3 which she had";
+
+const String dailychallenge1question1Wrong =
+    "No, that is not correct. Tina will have 7 hazelnuts, 4 from her mom and 3 which she had earlier";
+
+const List<String> dailychallenge1options1 = [
+  "2",
+  "3",
+  "7",
+  "10",
+];
+
+const List<String> dailychallenge1question2 = [
+  "Joe, the camel lives in the desert where water is not available frequently. Hence Joe drinks more water than it wants and stores in its hump for further use. Similarly it drinks 12.5 litres of water one day and stores it in its hump. If Joe uses 2.5 litres of water per day from its hump how many days can it live without water?",
+];
+
+const String dailychallenge1question2Correct =
+    "Yup, you are spot on!, Joe will have stored water for 5 days if he uses 2.5 liters of water per day";
+
+const String dailychallenge1question2Wrong =
+    "No, that is not correct. If Joe uses 2,5 liters per day and has stored 12,5 liters it would mean Joe has stored water for exactly 5 days as 12,5/2.5 = 5";
+
+const List<String> dailychallenge1options2 = [
+  "7 days",
+  "5 days",
+  "6 days",
+  "8 days",
+];
